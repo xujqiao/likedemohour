25ec96fc08c99440aacc07a96fde40011ffc8257
Heo-jq <snow8321@qq.com>
2014-03-25 12:08:59 +0800

    bootstrap for login and add

--- App/Runtime/Cache/18338f9b7b2547af2064e894f6d24326.php
+++ App/Runtime/Cache/18338f9b7b2547af2064e894f6d24326.php
@@ -1,38 +1,42 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<!-- saved from url=(0032)http://goldencis.cn/z/cis-login/ -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=GBK">
-<meta charset="gb2312">
-<title>登录</title>
-<link rel="stylesheet" type="text/css" href="/beginning/App/Tpl/Item/index/login.css">
-<script type="text/javascript" src="/beginning/App/Tpl/Item/index/jquery-1.8.3.min.js"></script><style type="text/css"></style>
-<script type="text/javascript" src="/beginning/App/Tpl/Item/index/login.js"></script> 
-</head>
-
-<body style="">
-<div> 
-    <div class="t_logo"></div>
-    <div class="login_box"> 
-        <div class="login_row"> 
-            <div> 
-                <form name="flogin" id="flogin" method="post" action="__URL__/login">
-                    <div class="f_row_name"> 
-                        <input id="fname" name="mail" type="email" class="fm_user"> 
+<html>
+    <head>
+        <title>登陆</title>
+        <link href="/beginning/Public/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+        <script type="text/javascript" src="/beginning/Public/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="/beginning/Public/bootstrap/js/bootstrap.min.js"></script>
+        <style type="text/css">
+            #login{
+                width: 500px;
+                height: 150px;
+                margin: 250px auto;
+                padding: 5px;
+            }
+            label{
+                font-size: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="login">
+            <form class="form-horizontal" method="post" action="__URL__/login">
+                <div class="control-group">
+                    <label class="control-label" for="inputEmail">邮箱</label>
+                    <div class="controls">
+                        <input type="email" id="inputEmail" name="mail" placeholder="Email"></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="inputPassword">密码</label>
+                    <div class="controls">
+                        <input type="password" id="inputPassword" name="pass" placeholder="Password"></div>
+                </div>
+                <div class="control-group">
+                    <div class="controls">
+                        <button type="submit" class="btn btn-large btn-primary">Sign in</button>
+                        <button type="reset" class="btn btn-large btn-primary">Reset</button>
                     </div>
-                    <div class="f_row_pwd"> 
-                       <input id="fpwd" name="pass" type="password" class="fm_pwd"> 
-                    </div>
-                    <div class="f_row_sub"> 
-                         <input id="sub" name="sub" type="submit" value="登录" class="fm_sub"> 
-                    </div> 
-                    <div id="logErr" class="f_row_err"></div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
-    </div>
-</div>
-
-
-
-
-
-<div id="transit-notify-list"><ul class="transit-list-inner"></ul></div><div class="vimiumReset vimiumHUD" style="right: 150px; opacity: 0; display: none;"></div></body></html>
+    </body>
+</html>
--- App/Runtime/Cache/20d5cab78a928416481d566a5faec4d4.php
+++ App/Runtime/Cache/20d5cab78a928416481d566a5faec4d4.php
@@ -1,12 +1,90 @@
-<?php if (!defined('THINK_PATH')) exit();?><html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+	<head>
+		<title>添加工程</title>
+		<meta name="viewport" content="width=device-witdh, initial-scale=1.0" />
+		<link href="/beginning/Public/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+		
+		<script type="text/javascript" src="/beginning/Public/jquery.js"></script>
+		<script type="text/javascript" src="/beginning/Public/bootstrap/js/bootstrap.min.js"></script>
+	</head>
 	<body>
-		<form method="post" action="__URL__/create" enctype="multipart/form-data">
-			工程名字:<input type="text" name="name" /><br />
-			工程简介:<textarea cols="30" rows="7" name="intro"></textarea><br />
-			工程具体介绍:<textarea cols="45" rows="15" name="introduction"></textarea><br />
-			需要的资金:<input type="text" name="money" /><br />
-			工程图片：<input type="file" name="pic" /><br />
-			<input type="submit" value="提交" /><input type="reset" value="重置" />
+		<!-- <form class="form-horizontal" method="post" action="__URL__/create" enctype="multipart/form-data">
+			<div>
+				<span class="control-group" >
+					<lable class="control-lable" for="inputName">工程名字</lable>
+					<input type="text" name="name" id="inputName" />
+				</span>
+			</div>
+			<br />
+			<div>
+				<span class="control-group" >
+					<lable class="control-lable" for="inputIntro">工程简介</lable>
+					<textarea cols="30" rows="7" name="intro" id="inputIntro" ></textarea>
+				</span>
+			</div>
+			<br />
+			<div>
+				<span class="control-group" >
+					<lable class="control-lable" for="inputIntroduction">工程具体简介</lable>
+					<textarea cols="45" rows="15" name="introduction" id="inputIntroduction" ></textarea>
+				</span>
+			</div>
+			<br />
+			<div>
+				<span class="control-group" >
+					<lable class="control-lable" for="inputMoney">需要的资金</lable>
+					<input type="text" name="money" id="inputMoney" />
+				</span>
+			</div>
+			<br />
+			<div>
+				<span class="control-group" >
+					<lable class="control-lable" for="inputPic">工程图片</lable>
+					<input type="file" name="pic" id="inputPic" />
+				</span>
+			</div>
+			<br />
+			<div class="form-acitons">
+				<button type="submit" class="btn btn-primary">提交</button>
+				<button type="reset" class="btn">重置</button>
+			</div>
+		</form> -->
+		<br />
+		<form class="form-horizontal" method="post" action="__URL__/create" enctype="multipart/form-data">
+			<div class="control-group">
+				<label class="control-label" for="inputName">工程名字</label>
+				<div class="controls">
+					<input type="text" id="inputName" placeholder="工程名字" name="name"></div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="inputIntro">工程简介</label>
+				<div class="controls">
+					<textarea id="inputIntro" placeholder="工程简介" rows="5" name="intro"></textarea></div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="inputIntroduction">工程具体简介</label>
+				<div class="controls">
+					<textarea id="inputIntroduction" placeholder="工程具体简介" rows="10" name="introduction"></textarea></div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="inputMoney">工程金额</label>
+				<div class="controls">
+					<input type="text" id="inputMoney" placeholder="工程金额" name="money"></div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="inputPic">工程图片</label>
+				<div class="controls">
+					<input type="file" id="inputPic" name="pic"></div>
+			</div>
+			<div class="control-group">
+				<div class="controls">
+					<button type="submit" class="btn btn-primary">提交</button>
+					<button type="reset" class="btn">重置</button>
+				</div>
+			</div>
 		</form>
+
+
 	</body>
 </html>
